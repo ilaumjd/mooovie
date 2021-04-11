685eff1e85acf166f1a8ef2ffdf82d4fd8794121
ilaumjd <ilaumjd@gmail.com>
2021-04-11 13:04:15 +0700

    store token (temporary)

---
+++ Mooovie/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Mooovie
+//
+//  Created by aku pintar indonesia on 08/04/21.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct API {
+        static let baseURL = "https://api.themoviedb.org/3"
+        static let baseImageURL = "https://image.tmdb.org/t/p/w780"
+        static let key = "bdcaeb8767c4f076870e1576371c357e"
+    }
+    
+}
