11c19a6876f87b07d292a706b0d2af54c9c66942
ilaumjd <ilaumjd@gmail.com>
2021-04-28 15:10:46 +0700

    core data add & get

--- Mooovie/Screens/Detail/DetailViewModel.swift
+++ Mooovie/Screens/Detail/DetailViewModel.swift
@@ -39,6 +39,8 @@ extension DetailViewModel {
         service.fetchMovieDetail(movieId: movieId) { movie in
             if let movie = movie {
                 self.movie.accept(MovieViewModel(model: movie))
+                CoreDataManager.shared.addBookmark(id: movie.id ?? 0, title: movie.title ?? "", poster: movie.posterPath ?? "")
+                print(CoreDataManager.shared.getBookmarks())
             }
         }
     }
