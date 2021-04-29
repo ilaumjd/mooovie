86ffb942d3d4740200913a99a0f976460fae01fa
ilaumjd <ilaumjd@gmail.com>
2021-04-29 10:58:00 +0700

    test isMovieBookmarked

--- MooovieTests/MooovieTests.swift
+++ MooovieTests/MooovieTests.swift
@@ -7,26 +7,15 @@
 
 import XCTest
 
+@testable import Mooovie
+
 class MooovieTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_isMovieBookmarked() {
+        let movieId = 1234
+        XCTAssertFalse(CoreDataManager.shared.isBookmarked(id: movieId))
+        CoreDataManager.shared.addBookmark(id: movieId, title: "Mortal Kombat", poster: "")
+        XCTAssertTrue(CoreDataManager.shared.isBookmarked(id: movieId))
     }
 
 }
